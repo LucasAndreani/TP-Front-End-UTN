a8920c24b198ef2ab4412848be7cf4149c78992b
Lucas Andreani <lucas.andreani@icloud.com>
2023-07-26 00:20:02 -0300

    Trabajo Final

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,5 @@
 import Header from "./Header/Header";
-import Footer from "./Footer/footer";
+import Footer from "./Footer/Footer";
 import ProductCard from "./ProductCard/ProductCard";
 import Counter from "./Counter/Counter";
 import ProductCartCard from "./ProductCartCard/ProductCartCard";
